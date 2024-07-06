0f3f2b390465a5a3b6fc044351e057942dbba35f
Yair Hengali <yairhengali@gmail.com>
2024-07-07 02:23:04 +0300

    implemented add post

--- index.js
+++ index.js
@@ -3,13 +3,37 @@ import express from "express"
 const app = express();
 const port = 3000;
 
+let allPosts = [];
+
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
-})
+});
+
+let acs = ""
+acs.isE
+
+app.post("/post", (req, res) => {
+    const newPost = {
+        content: req.body["post-content"],
+        author: req.body["post-author"].length === 0 ? "anonimus" : req.body["post-author"],
+        dateCreatd: new Date().toLocaleString()
+    };
+
+    allPosts.push(newPost);
+    res.render("index.ejs", {posts: allPosts});
+});
+
+app.patch("/edit", (req, res) => {
+    // res.render("index.ejs");
+});
+
+app.delete("/delete", (req, res) => {
+    // res.render("index.ejs");
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
-})
+});
